8ad1f83683c944b0457bda134a9fdfaf6f6e3cfe
Mateusvr <mateusvr2005@outlook.com>
2023-04-17 20:45:49 -0300

    aula vite

--- react_dc/react_dc-vite/src/App.jsx
+++ react_dc/react_dc-vite/src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import './App.css'
 import {InputText} from './components/InputText'
+import { MyTitle } from './components/MyTitle'
+
+<MyTitle />
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Olá, mundo!</h1>
-
-      <InputText />
-      <input type="text" name="nome" id="nome" placeholder='nome' />
+      {/* <h1>Olá, mundo!</h1> */}
+    <MyTitle title="Olá, mundo!!" />
+      
+      <InputText
+      id='nome'
+      name='nome'
+      placeholder='nome' />
       <br />
-      <input type="text" name="sobrenome" id="sobrenome" placeholder='sobrenome' />
+      <InputText
+      id='sobrenome'
+      name='sobrenome'
+      placeholder='sobrenome' />  
+
     </>
   )
 }
